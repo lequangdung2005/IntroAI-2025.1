548856741825be9cf0efdaded99fc1b62d63d75a
Do Hepha <trantrunghieudo@gmail.com>
2025-10-30 03:31:08 +0700

    New version of reward shaping wrapper - DQN 1M steps

--- shaping_reward/shaping_reward_dqn.py
+++ shaping_reward/shaping_reward_dqn.py
@@ -108,7 +108,7 @@ def train_dqn():
 
     # Train the agent
     model.learn(
-        total_timesteps=10000000,
+        total_timesteps=1000000,
         callback=[checkpoint_callback, eval_callback],
         log_interval=10,
         progress_bar=True
